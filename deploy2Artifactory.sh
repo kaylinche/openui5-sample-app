5e9d21edb14f72cd94408844a71d60698be3a4a5
Kay Wegner <kay.wegner@sap.com>
2023-04-20 20:32:19 +0200

    Update deploy2Artifactory.sh

--- deploy2Artifactory.sh
+++ deploy2Artifactory.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat > ~/.npmrc <<EOF
-${artifatctoryRegistryURL}/${artifactoryArtifact}/:_auth = ${artifactoryRegistryAuth_password}
+_auth = ${artifactoryRegistryAuth_password}
 always-auth = true
 email = ${artifactoryRegistryAuth_user}
 EOF
